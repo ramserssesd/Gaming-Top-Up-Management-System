dd1e38fc916d0e17194251bcb4b17b13f4efd1aa
ramserssesd <118060137+ramserssesd@users.noreply.github.com>
2025-06-17 06:08:14 +0530

    Update settings.py

--- gaming_topup_project/settings.py
+++ gaming_topup_project/settings.py
@@ -133,7 +133,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',  # ✅ THIS LINE IS REQUIRED
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
